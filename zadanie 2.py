0c8366452744daef687bbc25c7ae79330bee4699
Kinowski-Daniel <89976283+Kinowski-Daniel@users.noreply.github.com>
2022-03-31 12:52:29 +0200

    Update zadanie 2.py

--- zadanie 2.py
+++ zadanie 2.py
@@ -4,10 +4,8 @@ import numpy as np
 z = ["Włochy","Węgry","Szwecja","Słowenia","Słowacja","Rumunia","Portugalia","Polska","Niemcy","Malta","Łotwa","Luksemburg","Litwa","Irlandia","Holandia","Hiszpania","Grecja","Francja","Finlandia","Estonia","Dania","Czechy","Cypr","Chorwacja","Bułgaria","Belgia","Austria"]
 w = [1672438,112399,462057,39769,80958,169578,184931,424269,3134070,9898,25021,54195,38637,265835,697219,1113851,175888,2228857,214062,20916,276805,174412,17901,45557,47364,421611,349344]
 
-plt.rcdefaults()
+
 fig, ax = plt.subplots()
-ax.barh(z, w, xerr=len(z) )
-
-
+plt.bar(z, w)
 plt.show()
 
